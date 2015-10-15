c93904bbaf8fb42be3448ff6d7dcadc764074b2f
Jukka Hyvärinen <jukka.hyvarinen@solita.fi>
2015-10-15 17:24:44 +0300

    assembly info

--- Solita.Testing.EPiServer/Properties/AssemblyInfo.cs
+++ Solita.Testing.EPiServer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Solita.Testing.EPiServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for automated testing of EPiServer websites")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Solita")]
 [assembly: AssemblyProduct("Solita.Testing.EPiServer")]
