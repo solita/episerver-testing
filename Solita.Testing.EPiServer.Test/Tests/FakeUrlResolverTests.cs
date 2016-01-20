7801676230bd3ff4efd7098a8bd4b5a7d27a7b18
Jukka Hyvärinen <jukka.hyvarinen@solita.fi>
2016-01-20 13:04:02 +0200

    tests for FakeUrlResolver

---
+++ Solita.Testing.EPiServer.Test/Tests/FakeUrlResolverTests.cs
@@ -0,0 +1,40 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solita.Testing.EPiServer.Test.Tests
+{
+
+    /// <summary>
+    /// Tests for <see cref="FakeUrlResolver"/>.
+    /// </summary>
+    [TestClass]
+    public class FakeUrlResolverTests
+    {
+
+        private readonly FakeContentRepository contentRepository = new FakeContentRepository();
+
+        /// <summary>
+        /// Test routing content to URL and back.
+        /// </summary>
+        [TestMethod]
+        public void Route_WithContentRepository()
+        {
+            
+            var content = contentRepository.GetDefault<PageData>(ContentReference.EmptyReference);
+            content.Name = "Test content";
+            var contentReference = contentRepository.Save(content);
+            var resolver = new FakeUrlResolver(contentRepository);
+            var url = resolver.GetUrl(contentReference);
+
+            var routedContent = resolver.Route(new UrlBuilder(url));
+            
+            Assert.IsNotNull(routedContent, "routedContent");
+            Assert.AreEqual(contentReference, routedContent.ContentLink, "Routed content reference");
+            Assert.AreEqual("Test content", routedContent.Name, "Routed content name");
+
+        }
+
+    }
+
+}
